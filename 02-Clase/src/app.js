38b73bd3ec99d833654c2441cd2a9c9c5b5bc1d4
Diego Hdez <tsu.diego.hdezm@gmail.com>
2026-03-06 17:26:36 -0600

    axios

--- 02-Clase/src/app.js
+++ 02-Clase/src/app.js
@@ -1,22 +1,40 @@
-const {emailTemplate} = require('./js-foundation/01-template');
-const {getNamesById} = require('./js-foundation/03-callbacks');
-require('./js-foundation/05-factory');
-const {getAge, getId} = require('./plugins');
-
-//console.log(emailTemplate);
-const id = 2;
-getNamesById(id, function(error, user) {
-    if (error) {
-        throw new Error('USER  NOT FOUND WITH ID ', id);
-    }
-    //console.log(user);
+const getPokemonById = require('./js-foundation/06-promises');
+getPokemonById(4).then((pokemonName) => {
+    console.log(pokemonName);
 });
 
-const {buildMakePerson} = require('./js-foundation/05-factory');
-const makePerson = buildMakePerson({ getAge, getId });
-const obj = {name: 'Diego', birthdate: '1992-07-20'};
-const john = makePerson(obj);
-console.log(john);
+
+
+
+
+
+
+
+
+
+
+// const {emailTemplate} = require('./js-foundation/01-template');
+// const {getNamesById} = require('./js-foundation/03-callbacks');
+// require('./js-foundation/05-factory');
+// const {getAge, getId} = require('./plugins');
+
+// //console.log(emailTemplate);
+// const id = 2;
+// getNamesById(id, function(error, user) {
+//     if (error) {
+//         throw new Error('USER  NOT FOUND WITH ID ', id);
+//     }
+//     //console.log(user);
+// });
+
+// const {buildMakePerson} = require('./js-foundation/05-factory');
+// const makePerson = buildMakePerson({ getAge, getId });
+// const obj = {name: 'Diego', birthdate: '1992-07-20'};
+// const john = makePerson(obj);
+// console.log(john);
+
+
+
 
 
 
--- 02-Clase/src/plugins/index.js
+++ 02-Clase/src/plugins/index.js
@@ -1,7 +1,9 @@
 const { getId } = require('./get-id.plugin');
 const { getAge } = require('./get-age.plugin');
+const { httpClientPlugin } = require('./http-clientplugin');
 
 module.exports = {
     getId,
     getAge,
+    httpClientPlugin,
 }
